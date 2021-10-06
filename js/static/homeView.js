39d2d0961eec2658c4dc9f1231bfc6043b1d547c
Erik Ayala <ayala.erik61@gmail.com>
2021-10-05 21:42:01 -0500

    added files for front end

---
+++ js/static/homeView.js
@@ -0,0 +1,12 @@
+export default function Home(props) {
+    console.log("We out here!")
+    return `
+        <div><h1>How are we doing?</h1></div>
+   
+    `;
+}
+
+export function homeEvent() {
+
+
+}
